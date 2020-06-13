26f36052cc940771fe9a61c13fa5a9bb543fd10a
javi-acuna <javi.emiliano@gmail.com>
2020-06-13 19:47:18 -0300

    condiciones

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+// function SaludarEnIdiomas(idioma){
+//   if(idioma === "es"){
+//     return <span>Hola</span>;
+//     } else {
+//       return <span>Hello</span>;
+//     }
+// }
 
 const Saludo = () => {
   const nombre = 'Javier';
-  return <p>Hola { `${nombre}` }</p>;
+//  return <p><SaludarEnIdiomas idioma='en'/> {`${nombre}`}</p>
+  const idioma = "es";
+  return (
+    <div>
+    {
+      idioma === "en" ? <p>Hello</p> : <p>Hola</p>
+    }
+    </div>
+  )
 }
+
 const App = () =>{
   return <h1> <Saludo/> </h1>;
 }
