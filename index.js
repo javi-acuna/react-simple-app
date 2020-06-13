3474516106468522ffdbdf50f3e72674dde939f0
javi-acuna <javi.emiliano@gmail.com>
2020-06-13 19:30:39 -0300

    mostrar componentes

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+
+const Saludo = () => {
+  return <p>Hola</p>;
+}
 const App = () =>{
-  return <h1> Hola mundo!! </h1>
+  return <h1> <Saludo/> </h1>;
 }
 
-const Btn = ()=>{
-  return React.createElement("button",{
-    onClick: ()=> alert("Hola")
-  },"Enviar");
-}
+// const Btn = ()=>{
+//   return React.createElement("button",{
+//     onClick: ()=> alert("Hola")
+//   },"Enviar");
+// }
+
+//con jsx
+const Btn = () => <button onClick={()=> alert("Hola") }> Enviar </button>
 
 render(<App/>, document.getElementById('root'));
 render(<Btn/>, document.getElementById('button'));
