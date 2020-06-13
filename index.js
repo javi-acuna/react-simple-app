975dd8560730b258e693f8a75e77943cfee56c2a
javi-acuna <javi.emiliano@gmail.com>
2020-06-13 18:58:57 -0300

    se agrega boton

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const App = () =>{
   return <h1> Hola mundo!! </h1>
 }
 
+const Btn = ()=>{
+  return React.createElement("button",{
+    onClick: ()=> alert("Hola")
+  },"Enviar");
+}
+
 render(<App/>, document.getElementById('root'));
+render(<Btn/>, document.getElementById('button'));
